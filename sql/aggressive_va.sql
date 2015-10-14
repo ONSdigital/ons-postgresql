d5573390e64bcc947e10a2d6770647198150b70e
Steve Bennett <steven.bennett@ons.gsi.gov.uk>
2015-10-14 10:31:55 +0100

    Create aggressive_va.sql

---
+++ sql/aggressive_va.sql
@@ -0,0 +1,45 @@
+-- Alter autovacuum settings on certain EDC tables
+-- To be used only until data model is fixed.
+-- Note: this is not a long term solution I propose but a kludge.
+
+ALTER TABLE edc_sample_selection SET (
+  autovacuum_vacuum_threshold = 25,
+  autovacuum_vacuum_scale_factor = 0.1,
+  autovacuum_analyze_threshold = 10,
+  autovacuum_analyze_scale_factor = 0.05);
+
+ALTER TABLE edc_enrolment SET (
+  autovacuum_vacuum_threshold = 25,
+  autovacuum_vacuum_scale_factor = 0.1,
+  autovacuum_analyze_threshold = 10,
+  autovacuum_analyze_scale_factor = 0.05);
+
+ALTER TABLE edc_sampling_frame_inclusion SET (
+  autovacuum_vacuum_threshold = 25,
+  autovacuum_vacuum_scale_factor = 0.1,
+  autovacuum_analyze_threshold = 10,
+  autovacuum_analyze_scale_factor = 0.05);
+
+ALTER TABLE edc_instrument_useage SET (
+  autovacuum_vacuum_threshold = 25,
+  autovacuum_vacuum_scale_factor = 0.1,
+  autovacuum_analyze_threshold = 10,
+  autovacuum_analyze_scale_factor = 0.05);
+
+ALTER TABLE edc_instrument_version SET (
+  autovacuum_vacuum_threshold = 25,
+  autovacuum_vacuum_scale_factor = 0.1,
+  autovacuum_analyze_threshold = 10,
+  autovacuum_analyze_scale_factor = 0.05);
+
+ALTER TABLE edc_sampling_frame SET (
+  autovacuum_vacuum_threshold = 25,
+  autovacuum_vacuum_scale_factor = 0.1,
+  autovacuum_analyze_threshold = 10,
+  autovacuum_analyze_scale_factor = 0.05);
+
+ALTER TABLE edc_survey SET (
+  autovacuum_vacuum_threshold = 25,
+  autovacuum_vacuum_scale_factor = 0.1,
+  autovacuum_analyze_threshold = 10,
+  autovacuum_analyze_scale_factor = 0.05);
